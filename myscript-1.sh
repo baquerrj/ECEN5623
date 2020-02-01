4c1d472294ea3b8daafc33cad1b3753291975df6
Roberto Baquerizo <roba8460@colorado.edu>
2020-01-31 21:17:44 -0700

    typo in script

--- myscript-1.sh
+++ myscript-1.sh
@@ -6,7 +6,7 @@ cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq > /sys/devices/system/
 cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq > /sys/devices/system/cpu/cpu0/cpufreq/scaling_setspeed
 echo 1 > /sys/devices/system/cpu/cpu0/online
 
-echo userspace > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo userspace > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
 cat /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
 cat /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq > /sys/devices/system/cpu/cpu1/cpufreq/scaling_setspeed
 echo 1 > /sys/devices/system/cpu/cpu1/online
