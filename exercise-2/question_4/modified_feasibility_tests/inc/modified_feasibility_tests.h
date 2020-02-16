63765cdf9b58bcb2c38caa721d5c43adad6595ea
Roberto Baquerizo <roba8460@colorado.edu>
2020-02-16 11:41:00 -0700

    new directory structure because the single source file was getting too long for my taste

---
+++ exercise-2/question_4/modified_feasibility_tests/inc/modified_feasibility_tests.h
@@ -0,0 +1,13 @@
+#ifndef MODIFIED_FEASIBILITY_TESTS_H
+#define MODIFIED_FEASIBILITY_TESTS_H
+
+#define U32_T unsigned int
+#define FALSE 0
+#define TRUE 1
+
+
+int completion_time_feasibility( U32_T numServices, U32_T period[], U32_T wcet[], U32_T deadline[] );
+int scheduling_point_feasibility( U32_T numServices, U32_T period[], U32_T wcet[], U32_T deadline[] );
+void print_set_information( U32_T numExercise, U32_T numServices, U32_T period[], U32_T wcet[], U32_T deadline[] );
+
+#endif // MODIFIED_FEASIBILITY_TESTS_H
--- exercise-2/question_4/modified_feasibility_tests/src/main.c
+++ exercise-2/question_4/modified_feasibility_tests/src/main.c
@@ -1,9 +1,7 @@
 #include <math.h>
 #include <stdio.h>
 
-#define TRUE 1
-#define FALSE 0
-#define U32_T unsigned int
+#include <modified_feasibility_tests.h>
 
 // U=0.7333
 U32_T ex0_period[] = {2, 10, 15};
@@ -50,9 +48,9 @@ U32_T ex8_wcet[]   = {1, 1, 1, 2};
 U32_T ex9_period[] = {6, 8, 12, 24};
 U32_T ex9_wcet[]   = {1, 2, 4, 6};
 
-int completion_time_feasibility( U32_T numServices, U32_T period[], U32_T wcet[], U32_T deadline[] );
-int scheduling_point_feasibility( U32_T numServices, U32_T period[], U32_T wcet[], U32_T deadline[] );
-void print_set_information( U32_T numExercise, U32_T numServices, U32_T period[], U32_T wcet[], U32_T deadline[] );
+//int completion_time_feasibility( U32_T numServices, U32_T period[], U32_T wcet[], U32_T deadline[] );
+//int scheduling_point_feasibility( U32_T numServices, U32_T period[], U32_T wcet[], U32_T deadline[] );
+//void print_set_information( U32_T numExercise, U32_T numServices, U32_T period[], U32_T wcet[], U32_T deadline[] );
 
 int main( void )
 {
@@ -194,134 +192,4 @@ int main( void )
    {
       printf( "INFEASIBLE\n" );
    }
-}
-
-int completion_time_feasibility( U32_T numServices,
-                                 U32_T period[],
-                                 U32_T wcet[],
-                                 U32_T deadline[] )
-{
-   int i, j;
-   U32_T an, anext;
-
-   // assume feasible until we find otherwise
-   int set_feasible = TRUE;
-
-   //printf("numServices=%d\n", numServices);
-
-   for ( i = 0; i < numServices; i++ )
-   {
-      an    = 0;
-      anext = 0;
-
-      for ( j = 0; j <= i; j++ )
-      {
-         an += wcet[ j ];
-      }
-
-      //printf("i=%d, an=%d\n", i, an);
-
-      while ( 1 )
-      {
-         anext = wcet[ i ];
-
-         for ( j = 0; j < i; j++ )
-            anext += ceil( ( (double)an ) / ( (double)period[ j ] ) ) * wcet[ j ];
-
-         if ( anext == an )
-            break;
-         else
-            an = anext;
-
-         //printf("an=%d, anext=%d\n", an, anext);
-      }
-
-      //printf("an=%d, deadline[%d]=%d\n", an, i, deadline[i]);
-
-      if ( an > deadline[ i ] )
-      {
-         set_feasible = FALSE;
-      }
-   }
-
-   return set_feasible;
-}
-
-int scheduling_point_feasibility( U32_T numServices,
-                                  U32_T period[],
-                                  U32_T wcet[],
-                                  U32_T deadline[] )
-{
-   int rc = TRUE, i, j, k, l, status, temp;
-
-   for ( i = 0; i < numServices; i++ )  // iterate from highest to lowest priority
-   {
-      status = 0;
-
-      for ( k = 0; k <= i; k++ )
-      {
-         for ( l = 1; l <= ( floor( (double)period[ i ] / (double)period[ k ] ) ); l++ )
-         {
-            temp = 0;
-
-            for ( j = 0; j <= i; j++ )
-               temp += wcet[ j ] * ceil( (double)l * (double)period[ k ] / (double)period[ j ] );
-
-            if ( temp <= ( l * period[ k ] ) )
-            {
-               status = 1;
-               break;
-            }
-         }
-         if ( status )
-            break;
-      }
-      if ( !status )
-         rc = FALSE;
-   }
-   return rc;
-}
-
-double calculate_utility( U32_T numServices, U32_T period[], U32_T wcet[] )
-{
-   int i          = 0;
-   double utility = 0.0;
-   for ( i = 0; i < numServices; ++i )
-   {
-      utility = utility + ( (double)wcet[ i ] / (double)period[ i ] );
-   }
-   return utility;
-}
-
-void print_set_information( U32_T numExercise,
-                            U32_T numServices,
-                            U32_T period[],
-                            U32_T wcet[],
-                            U32_T deadline[] )
-{
-   int i          = 0;
-   double utility = calculate_utility( numServices, period, wcet );
-   printf( "Ex-%u U=%4.2f (",
-           numExercise,
-           utility );
-   for ( i = 0; i < numServices; ++i )
-   {
-      printf( "C%u=%u%c ",
-              i + 1,
-              wcet[ i ],
-              ( i == ( numServices - 1 ) ? ';' : ',' ) );
-   }
-   U32_T periods_equal_deadlines = TRUE;
-   for ( i = 0; i < numServices; ++i )
-   {
-      printf( "T%u=%u%c ",
-              i + 1,
-              period[ i ],
-              ( i == ( numServices - 1 ) ? ';' : ',' ) );
-      if ( period[ i ] != deadline[ i ] )
-      {
-         periods_equal_deadlines = FALSE;
-      }
-   }
-   printf( "T%sD): ", ( ( periods_equal_deadlines == FALSE ) ? "/=" : "=" ) );
 }
