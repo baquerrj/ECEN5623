d9350962d935873e85fb34499a3ef21135a55372
Roberto Baquerizo <roba8460@colorado.edu>
2020-04-30 21:30:04 -0600

    makefile update

--- exercises/Time-Lapse_Image_Acquisition/Rules.mk
+++ exercises/Time-Lapse_Image_Acquisition/Rules.mk
@@ -23,28 +23,24 @@ targets :  $(TGT_LIB) $(TGT_BIN) $(TGT_SBIN) $(TGT_ETC)
 	@ln -f -t $(BIN) -s ../code/bin/client
 
 .PHONY : clean
-clean : clean_code
+clean : clean_code clean_docs
 	rm -rf $(BIN)
 	rm -f *log *ppm
 
 .PHONY : clean_obj
 clean_obj : clean_obj_clean
 
-.PHONY : update_all
-update_all : targets
+.PHONY : remote
+remote :
+	zip -r remote.zip *
 	ssh -tt jetsonnano  "mkdir -p ~/Time-Lapse_Image_Acquisition"
-	scp -r code/  jetsonnano:~/Time-Lapse_Image_Acquisition/
-	scp Makefile  jetsonnano:~/Time-Lapse_Image_Acquisition/
-	scp Rules.mk  jetsonnano:~/Time-Lapse_Image_Acquisition/
-
-.PHONY : update_server
-update_server : targets
-	ssh -tt jetsonnano  "mkdir -p ~/Time-Lapse_Image_Acquisition"
-	scp code/inc/*  jetsonnano:~/Time-Lapse_Image_Acquisition/code/inc/.
-	scp code/src/server/*  jetsonnano:~/Time-Lapse_Image_Acquisition/code/src/server/.
-	scp Makefile Rules.mk jetsonnano:~/Time-Lapse_Image_Acquisition/
-	scp code/*.mk  jetsonnano:~/Time-Lapse_Image_Acquisition/code/.
+	scp remote.zip jetsonnano:~/
+	ssh -tt jetsonnano "unzip remote.zip -d remote/"
 
 .PHONY : docs
 docs :
 	/usr/bin/doxygen docs/Doxyfile
+
+.PHONY : clean_docs
+clean_docs :
+	rm -rf docs/html docs/latex
