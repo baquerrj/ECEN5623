1df1f6ca508e1445bb19353e8d919cc44e0e07ef
Roberto Baquerizo <roba8460@colorado.edu>
2020-05-01 00:48:10 -0600

    server + cleanup service

--- exercises/Time-Lapse_Image_Acquisition/code/src/server/server.cpp
+++ exercises/Time-Lapse_Image_Acquisition/code/src/server/server.cpp
@@ -7,6 +7,7 @@
 #include <SocketBase.h>
 #include <SocketServer.h>
 #include <V4l2.h>
+#include <cleanup.h>
 #include <logging.h>
 #include <signal.h>
 #include <stdio.h>
@@ -20,10 +21,12 @@
 bool abortS1;
 bool abortS2;
 bool abortS3;
+bool abortS4;
 
 sem_t* semS1;
 sem_t* semS2;
 sem_t* semS3;
+sem_t* semS4;
 
 const char* host;
 utsname hostName;
@@ -32,6 +35,63 @@ uint32_t FRAMES_TO_EXECUTE = DEFAULT_FRAMES;
 logging::config_s config   = {logging::LogLevel::ERROR, "server.log"};
 
 pthread_mutex_t ringLock;
+
+bool usingCleanupThread = false;
+
+static void createSemaphoresAndMutexes()
+{
+   pthread_mutex_init( &ringLock, NULL );
+
+   semS1 = sem_open( SEMS1_NAME, O_CREAT | O_EXCL, 0644, 0 );
+   if ( semS1 == SEM_FAILED )
+   {
+      sem_unlink( SEMS1_NAME );
+      semS1 = sem_open( SEMS1_NAME, O_CREAT | O_EXCL, 0644, 0 );
+      if ( semS1 == SEM_FAILED )
+      {
+         perror( "Failed to initialize S1 semaphore" );
+         exit( -1 );
+      }
+   }
+
+   semS2 = sem_open( SEMS2_NAME, O_CREAT | O_EXCL, 0644, 0 );
+   if ( semS2 == SEM_FAILED )
+   {
+      sem_unlink( SEMS2_NAME );
+      semS2 = sem_open( SEMS2_NAME, O_CREAT | O_EXCL, 0644, 0 );
+      if ( semS2 == SEM_FAILED )
+      {
+         perror( "Failed to initialize S2 semaphore" );
+         exit( -1 );
+      }
+   }
+
+   semS3 = sem_open( SEMS3_NAME, O_CREAT | O_EXCL, 0644, 0 );
+   if ( semS3 == SEM_FAILED )
+   {
+      sem_unlink( SEMS3_NAME );
+      semS3 = sem_open( SEMS3_NAME, O_CREAT | O_EXCL, 0644, 0 );
+      if ( semS3 == SEM_FAILED )
+      {
+         perror( "Failed to initialize S3 semaphore" );
+         exit( -1 );
+      }
+   }
+   if ( usingCleanupThread )
+   {
+      semS4 = sem_open( SEMS4_NAME, O_CREAT | O_EXCL, 0644, 0 );
+      if ( semS4 == SEM_FAILED )
+      {
+         sem_unlink( SEMS4_NAME );
+         semS4 = sem_open( SEMS4_NAME, O_CREAT | O_EXCL, 0644, 0 );
+         if ( semS4 == SEM_FAILED )
+         {
+            perror( "Failed to initialize S3 semaphore" );
+            exit( -1 );
+         }
+      }
+   }
+}
 
 int main( int argc, char* argv[] )
 {
@@ -69,50 +129,24 @@ int main( int argc, char* argv[] )
    logging::configure( config );
    logging::INFO( "SERVER ON " + std::string( host ), true );
 
-   semS1 = sem_open( SEMS1_NAME, O_CREAT | O_EXCL, 0644, 0 );
-   if ( semS1 == SEM_FAILED )
-   {
-      sem_unlink( SEMS1_NAME );
-      semS1 = sem_open( SEMS1_NAME, O_CREAT | O_EXCL, 0644, 0 );
-      if ( semS1 == SEM_FAILED )
-      {
-         perror( "Failed to initialize S1 semaphore" );
-         exit( -1 );
-      }
-   }
-
-   semS2 = sem_open( SEMS2_NAME, O_CREAT | O_EXCL, 0644, 0 );
-   if ( semS2 == SEM_FAILED )
-   {
-      sem_unlink( SEMS2_NAME );
-      semS2 = sem_open( SEMS2_NAME, O_CREAT | O_EXCL, 0644, 0 );
-      if ( semS2 == SEM_FAILED )
-      {
-         perror( "Failed to initialize S2 semaphore" );
-         exit( -1 );
-      }
-   }
-
-   semS3 = sem_open( SEMS3_NAME, O_CREAT | O_EXCL, 0644, 0 );
-   if ( semS3 == SEM_FAILED )
-   {
-      sem_unlink( SEMS3_NAME );
-      semS3 = sem_open( SEMS3_NAME, O_CREAT | O_EXCL, 0644, 0 );
-      if ( semS3 == SEM_FAILED )
-      {
-         perror( "Failed to initialize S3 semaphore" );
-         exit( -1 );
-      }
-   }
-
    double serviceDeadline   = 1 / (double)captureFrequency;
    double sequencerDeadline = 1 / (double)Sequencer::SEQUENCER_FREQUENCY;
 
-   pthread_mutex_init( &ringLock, NULL );
-   FrameCollector* fc          = new FrameCollector( 0 );
-   FrameProcessor* fp          = new FrameProcessor( );
-   FrameSender* fs             = new FrameSender();
-   Sequencer* sequencer        = new Sequencer( captureFrequency );
+   usingCleanupThread = ( FRAMES_TO_EXECUTE > 2000 );
+   abortS4            = usingCleanupThread ? false : true;
+
+   createSemaphoresAndMutexes();
+
+   FrameCollector* fc    = new FrameCollector( 0 );
+   FrameProcessor* fp    = new FrameProcessor();
+   FrameSender* fs       = new FrameSender();
+   Sequencer* sequencer  = new Sequencer( captureFrequency );
+   Cleanup* cleanService = nullptr;
+   if ( not abortS4 )
+   {
+      // don't schedule cleanup with not doing more than 2000 frames
+      cleanService = new Cleanup( fc, fs );
+   }
    pthread_t sequencerThreadId = sequencer->getThreadId();
    pthread_t processorThreadId = fp->getThreadId();
    pthread_t collectorThreadId = fc->getThreadId();
@@ -141,6 +175,15 @@ int main( int argc, char* argv[] )
    pthread_join( senderThreadId, NULL );
    pthread_join( processorThreadId, NULL );
    pthread_join( collectorThreadId, NULL );
+
+   if ( not abortS4 and ( cleanService != nullptr ) )
+   {
+      sem_post( semS4 );
+      abortS4 = true;
+      delete cleanService;
+      sem_close( semS4 );
+      sem_unlink( SEMS4_NAME );
+   }
 
    fc->terminate();
 
